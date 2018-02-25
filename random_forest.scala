db3cb4bbe59df239e6803263d2f908406ee0e9af
charles jekal <charles.jekal@ankura.com>
2018-02-24 21:13:45 -0800

    adding binary classifier metrics

--- random_forest.scala
+++ random_forest.scala
@@ -89,3 +89,53 @@ true positive   false positive
 false negative  true negative
 */
 evaluationMetrics.confusionMatrix
+
+/*
+More metrics, this was copied from
+https://spark.apache.org/docs/2.2.1/mllib-evaluation-metrics.html#binary-classification
+*/
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+
+// Instantiate metrics object
+val metrics = new BinaryClassificationMetrics(labeledPredictions)
+
+// Precision by threshold
+val precision = metrics.precisionByThreshold
+precision.foreach { case (t, p) =>
+  println(s"Threshold: $t, Precision: $p")
+}
+
+// Recall by threshold
+val recall = metrics.recallByThreshold
+recall.foreach { case (t, r) =>
+  println(s"Threshold: $t, Recall: $r")
+}
+
+// Precision-Recall Curve
+val PRC = metrics.pr
+
+// F-measure
+val f1Score = metrics.fMeasureByThreshold
+f1Score.foreach { case (t, f) =>
+  println(s"Threshold: $t, F-score: $f, Beta = 1")
+}
+
+val beta = 0.5
+val fScore = metrics.fMeasureByThreshold(beta)
+f1Score.foreach { case (t, f) =>
+  println(s"Threshold: $t, F-score: $f, Beta = 0.5")
+}
+
+// AUPRC
+val auPRC = metrics.areaUnderPR
+println("Area under precision-recall curve = " + auPRC)
+
+// Compute thresholds used in ROC and PR curves
+val thresholds = precision.map(_._1)
+
+// ROC Curve
+val roc = metrics.roc
+
+// AUROC
+val auROC = metrics.areaUnderROC
+println("Area under ROC = " + auROC)
